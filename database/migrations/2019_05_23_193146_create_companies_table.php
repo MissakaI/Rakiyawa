9f803494aaebf83be6af4c9c36065ddd01c17414
MissakaI <m.iddamalgoda@gmail.com>
2019-05-24 01:34:05 +0530

    Initial Commit with Laravel

---
+++ database/migrations/2019_05_23_193146_create_companies_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('registration_no');
+            $table->string('name');
+            $table->string('hq_address');  //can be broken into further columns later
+            $table->string('');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+}
