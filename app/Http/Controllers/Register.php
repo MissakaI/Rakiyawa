2c616a69fb37cedccb1eba0a4421b793e9dd2921
Ilthizam <ilthizambest@gmail.com>
2019-07-05 14:58:43 +0530

    user login fucntion created

--- app/Http/Controllers/Register.php
+++ app/Http/Controllers/Register.php
@@ -2,20 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
-use\App\Member;
+use \App\Member;
 
 class Register extends Controller
 {
-    public function reg(Request $request){
-       
-        return view('register');
-     }
+    public function reg(Request $request)
+    {
 
-     public function store(Request $request){
-        
+        return view('register');
+    }
+
+    public function store(Request $request)
+    {
+
         $member = new Member;
-    
+
         $member->first_name = $request->firstname;
         $member->last_name = $request->lastname;
         $member->email = $request->email;
@@ -25,15 +28,36 @@ class Register extends Controller
         $member->password = $request->password;
         $member->biography = $request->Biography;
         $member->website = $request->Website;
-        $member->contact_info = '{"number":"'.$request->Contact_Info.'"}';
+        $member->contact_info = '{"number":"' . $request->Contact_Info . '"}';
         $member->recommendations = '{"recommendation":"no"}';
         $member->profile_picture = " ";
         $member->save();
-        
-        $data = Member::all();
-        dd($data);
-        
-        
-     }
 
+        // return redirect('');
+        // $data = Member::all();
+        // dd($data);
+
+
+    }
+    public function login(Request $request)
+    {
+        $username = $request->get('login-username');
+        $password = $request->get('login-password');
+
+
+        $data = DB::table('members')->where('username', $username)->first();
+        if ($data != null) {
+            if ($password == $data->password) {
+                $request->session()->put('user_id', $data->id);
+                return redirect( '/user/'.$data->id);
+
+            } else {
+                // return redirect('companyloginpage');
+                echo "Fail";
+            }
+        } else {
+            // return redirect('companyloginpage');
+            echo "Fail";
+        }
+    }
 }
